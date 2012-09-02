02984c2f5666dee84991b11447a858f10cd4b1c6
Lance Wicks <lw@judocoach.com>
2012-09-02 23:30:01 +0100

    Broke decoding the Ippon.org protocol v 040 into a function

--- udp_server.js
+++ udp_server.js
@@ -5,25 +5,21 @@ var server = dgram.createSocket("udp4");
 // ------------------
 // vars for data
 // -----------------
-var clock;
-var white_score = "";
-var blue_score = "";
-
-var old_white_score = "";
-var old_blue_score = "";
+var old_data;
 
 
 server.on("message", function (data, rinfo) {
   msg = data.toString();
-  clock = msg.slice(35,36) + ":" + msg.slice(36,38);
-  white_score = msg.slice(93,97);
-  blue_score = msg.slice(155,159);
-  if(white_score != old_white_score)
-  {
-      console.log(clock +" "+white_score+":"+blue_score);
-      old_white_score = white_score;
-      old_blue_score = blue_score;
-  }
+  var data = ParseMsg(msg);
+
+
+  if(data.ProtoVer == '040'){
+    if(data.TimerFlag == 1) {
+      data_old = data;
+      console.log("(Mat "+data.MatSending+") "+data.TimerMinute+":"+data.TimerSecond+" "+data.NameWhiteLong+" vs. "+data.NameBlueLong);	
+    }
+  }  
+ 
 });
 
 server.on("listening", function () {
@@ -31,5 +27,52 @@ server.on("listening", function () {
   console.log("server listening " +
       address.address + ":" + address.port);
 });
+
+function ParseMsg(msg) {
+  var data = new Object();
+  msg = "." + msg;
+  data.ProtoVer       	= msg.substr(2, 3);
+  data.IDEvent        	= msg.substr(5, 20);
+  data.Gender         	= msg.substr(25, 1);
+  data.Category       	= msg.substr(26, 4);
+  data.AgeGroup       	= msg.substr(30, 1);
+  data.Round          	= msg.substr(21, 1);
+  data.ContestID      	= msg.substr(32, 3);
+  data.TimerFlag      	= msg.substr(35, 1);
+  data.TimerMinute    	= msg.substr(36, 1);
+  data.TimerSecond    	= msg.substr(37, 2);
+  data.NationWhite    	= msg.substr(39, 3);
+  data.IDWhite        	= msg.substr(42, 15);
+  data.NameWhiteShort 	= msg.substr(57, 4);
+  data.WRLWhite       	= msg.substr(61, 3);
+  data.NameWhiteLong  	= msg.substr(64, 30);
+  data.IpponWhite     	= msg.substr(94, 1);
+  data.WazaWhite      	= msg.substr(95, 1);
+  data.YukoWhite      	= msg.substr(96, 1);
+  data.PenaltyWhite   	= msg.substr(97, 1);
+  data.TimerOasaeWhite 	= msg.substr(98, 1);
+  data.TeamScoreWhite  	= msg.substr(100, 1);
+  data.NationBlue	= msg.substr(101, 3);
+  data.IDBlue		= msg.substr(104, 15);
+  data.NameBlueShort	= msg.substr(119, 4);
+  data.WRLBlue		= msg.substr(123, 3);
+  data.NameBlueLong	= msg.substr(126, 30);
+  data.IpponBlue	= msg.substr(156, 1);
+  data.WazaBlue		= msg.substr(157, 1);
+  data.YukoBlue		= msg.substr(158, 1);
+  data.PenaltyBlue	= msg.substr(159, 1);
+  data.TimerOsaeBlue	= msg.substr(160, 2);
+  data.TeamScoreBlue	= msg.substr(162, 1);
+  data.GoldenScore	= msg.substr(163, 1);
+  data.Winner		= msg.substr(164, 1);
+  data.IDReferee	= msg.substr(165, 15);
+  data.IDJudge1		= msg.substr(180, 15);
+  data.IDJudge2		= msg.substr(195, 15);
+  data.MatSending	= msg.substr(210, 1);
+  data.DisplayMode	= msg.substr(211, 1);
+ 
+  return(data);
+}
+
 
 server.bind(4001);
